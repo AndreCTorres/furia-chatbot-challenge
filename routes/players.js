875a4fbd6314902207640af136dc6505ae731d53
André Torres <andrectorres17@gmail.com>
2025-05-04 23:08:11 -0300

    Atualização para obter dados dos players

--- routes/players.js
+++ routes/players.js
@@ -1,10 +1,17 @@
 const express = require('express');
+const path = require('path');
+const fs = require('fs');
 const router = express.Router();
-const dataLoader = require('../dataLoader');
 
-// Todos os jogadores do time (Titular, Reserva, Técnico)
 router.get('/players', (req, res) => {
-    res.json(dataLoader.data.players || []);
+    const filePath = path.join(__dirname, '../static/data/players.json');
+    fs.readFile(filePath, 'utf8', (err, data) => {
+        if (err) {
+            console.error('Erro ao ler players.json:', err);
+            return res.status(500).json({ erro: 'Erro ao carregar jogadores' });
+        }
+        res.json(JSON.parse(data));
+    });
 });
 
 module.exports = router;
